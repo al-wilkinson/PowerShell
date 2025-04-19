a0b1ac8b8d2a8aa7a5d80f51f78c1261a1998c63
al-wilkinson <82517689+al-wilkinson@users.noreply.github.com>
2025-04-19 20:19:15 +1000

    Update IPv4-to-int.ps1

--- IPv4-to-int.ps1
+++ IPv4-to-int.ps1
@@ -4,29 +4,33 @@ function IPv4_to_Int {
         [string]$ip
     )
 
-    $octets = $ip.Split(".") | ForEach-Object {[int32]$_}
-    # $octets
-    $octets.GetType()
+    $octets = $ip.Split(".") | ForEach-Object {[int64]$_}
     
-    $ipInt = ($octets[0] * 16777216) + ($octets[1] * 65536) + ($octets[2] * 256) + $octets[3]
-    # $ipInt
+    $ipInt = ($octets[3] * 16777216) + ($octets[2] * 65536) + ($octets[1] * 256) + $octets[0]
     return $ipInt
 }
 
 function Int_to_IPv4 {
     param (
         [Parameter(Mandatory=$true)]
-        [int]$ipInt
+        [int64]$ipInt
     )
 
-    $ipInt/16777216
-    
+    $octet1 = $ipInt%256
+    $octet2 = (($ipInt%65536)-$octet1)/256
+    $octet3 = (($ipInt%16777216)-$octet2*256-$octet1)/65536
+    $octet4 = (($ipInt-$octet3*65536-$octet2*256-$octet1)/16777216)
+
+    $octet1
+    $octet2
+    $octet3
+    $octet4   
 }
 
-$ip = "192.168.2.1"
+$ip = "100.251.244.254"
 $ipInt = IPv4_to_Int -ip $ip
 Write-Host "Integer for '$ip' is: " $ipInt
-$ipInt.GetType()
+# $ipInt.GetType()
 
 $reversedInt_to_IP = Int_to_IPv4 -ipInt $ipInt
 $reversedInt_to_IP
